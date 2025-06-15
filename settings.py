aec12ee8dc2bdc1dd9656bb8469f259061525009
CamilaCristina2 <a2023110951@alumni.iscac.pt>
2025-06-15 22:39:59 +0100

    Adiciona dump.sql com a base de dados lojacoverde

--- settings.py
+++ settings.py
@@ -67,21 +67,20 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
-from decouple import config
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
+        'NAME': 'lojacoverde',  # EXATAMENTE como está no MySQL
+        'USER': 'root',
+        'PASSWORD': '0000',  # Substitua pela senha real
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
-
-    
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
